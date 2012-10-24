eb810deb67aa056f495e99dd39cc77a1bdfcec84
Administrator <Administrator@MSDN-SPECIAL>
2012-10-24 15:18:22 +0900

    번역관련 기능 추가

--- src/main/java/com/ctb/pilot/common/util/JsonUtils.java
+++ src/main/java/com/ctb/pilot/common/util/JsonUtils.java
@@ -1,11 +1,14 @@
 package com.ctb.pilot.common.util;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
+import org.codehaus.jackson.type.TypeReference;
 
 public class JsonUtils {
 
@@ -15,5 +18,13 @@ public class JsonUtils {
 		ObjectWriter objectWriter = objectMapper.writer();
 		return objectWriter.writeValueAsString(object);
 	}
-
+	
+	public static String getProperty(String key, String jsonString)
+			throws JsonParseException, JsonMappingException, IOException {
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		Map<String,String> map = objectMapper.readValue(jsonString,
+				new TypeReference<Map<String, String>>(){});
+		return map.get(key);
+	}
 }
