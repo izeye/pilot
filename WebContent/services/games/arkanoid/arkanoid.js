d403cced06682a4aefcf344c2cb19b60b403b144
izeye <izeye@naver.com>
2013-03-10 11:32:34 +0900

    Implement Arkanoid.

--- WebContent/services/games/arkanoid/arkanoid.js
+++ WebContent/services/games/arkanoid/arkanoid.js
@@ -2,30 +2,43 @@
 	document = window.document;
 	canvas = document.getElementById('canvas');
 	context = canvas.getContext('2d');
-
+	
+	var bat = {};
+	bat.width = 80;
+	bat.height = 20;
+	bat.x = 0;
+	bat.y = canvas.height - bat.height;
+	bat.color = 'yellow';
+	bat.init = function () {
+		bat.x = canvas.width / 2 - bat.width / 2;
+	};
+	
+	var ball = {};
+	ball.radius = 10;
+	ball.x = 0;
+	ball.y = 0;
+	ball.dx = -2;
+	ball.dy = -4;
+	ball.color = '#000';
+	ball.init = function () {
+		ball.x = bat.x + bat.width / 2 - ball.radius;
+		ball.y = canvas.height - bat.height - ball.radius;
+	};
+	
+	level = 1;
+	
+	timer_id = 0;
+	
 	ARKANOID = {
-		bat: {
-			width: 80,
-			height: 20,
-			x: canvas.width / 2 - this.width / 2,
-			y: canvas.height - this.height
-		},
-		ball: {
-			radius: 10,
-			x: this.bat.x + this.bat.width / 2 - this.radius,
-			y: this.canvas.height - this.bat.height - this.radius,
-			dx: 0,
-			dy: 0
-		},
 		init: function () {
-			console.log('init');
-			this.bind();
+			var self = this;
 			
-			this.context.fillStyle = '#CCC';
-			this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+			console.log('init');
+			
+			self.bind();
 		},
 		bind: function () {
-			this.document.onkeydown = function (e) {
+			document.onkeydown = function (e) {
 				var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
 				switch (key) {
 					case 37: // arrow left
@@ -39,8 +52,64 @@
 			};
 		},
 		start: function () {
+			var self = this;
+			
 			console.log('start');
-			this.init();
+			
+			// FIXME: doesn't appear.
+			self.print('Arkanoid');
+			
+			self.clearScreen();
+
+			bat.init();
+			ball.init();
+			
+			self.print('Level ' + level);
+			
+			window.setTimeout(function () {
+				timer_id = window.setInterval(function () {
+					console.log(self);
+					self.refreshFrames.call(self);
+				}, 10);
+			}, 2000);
+		},
+		print: function(text) {
+			context.textAlign = 'center';
+			context.font = 'bold 20px sans-serif';
+			context.fillStyle = '#000';
+			context.fillText(text, canvas.width / 2, canvas.height / 2, canvas.width);
+		},
+		clearScreen: function () {
+//			context.clearRect(0, 0, canvas.width, canvas.height);
+			
+			context.fillStyle = '#CCC';
+			context.fillRect(0, 0, canvas.width, canvas.height);
+		},
+		refreshFrames: function () {
+			var self = this;
+			
+			self.clearScreen();
+			
+			self.drawBall();
+			self.drawBat();
+		},
+		drawBall: function () {
+//			context.strokeStyle = ball.color;
+			context.fillStyle = ball.color;
+			context.beginPath();
+			context.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2, true);
+			context.closePath();
+//			context.stroke();
+			context.fill();
+		},
+		drawBat: function () {
+			context.fillStyle = bat.color;
+			context.beginPath();
+			context.rect(bat.x, bat.y, bat.width, bat.height);
+			context.closePath();
+			context.fill();
 		}
 	};
+	
+	ARKANOID.init();
 }());
