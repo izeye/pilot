74f8a17cf3be8d011e99b70ebac47b2064f83d1c
izeye <izeye@naver.com>
2013-09-08 11:12:59 +0900

    Commit.

---
+++ WebContent/WEB-INF/views/pilot/sub_menu.jsp
@@ -0,0 +1,7 @@
+<div class="well sidebar-nav">
+	<ul class="nav nav-list">
+		<li class="nav-header">Algorithm Contest</li>
+		<li><a href="/pilot/algorithm_contest/programming_challenge">Programming Challenge</a></li>
+		<li class="divider"></li>
+	</ul>
+</div>
--- src/main/java/com/ctb/pilot/common/model/Country.java
+++ src/main/java/com/ctb/pilot/common/model/Country.java
@@ -2,9 +2,9 @@ package com.ctb.pilot.common.model;
 
 public class Country {
 
-	private final String iso;
-	private final String code;
-	private final String name;
+	private String iso;
+	private String code;
+	private String name;
 
 	public Country(String iso, String code, String name) {
 		this.iso = iso;
@@ -12,16 +12,31 @@ public class Country {
 		this.name = name;
 	}
 
+	public Country() {
+	}
+
 	public String getIso() {
 		return iso;
+	}
+
+	public void setIso(String iso) {
+		this.iso = iso;
 	}
 
 	public String getCode() {
 		return code;
 	}
 
+	public void setCode(String code) {
+		this.code = code;
+	}
+
 	public String getName() {
 		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	@Override
